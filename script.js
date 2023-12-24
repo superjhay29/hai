9024ba666f5f056f9ad66543d8e78bab2da8f49f
superjhay29 <105788845+superjhay29@users.noreply.github.com>
2023-12-24 22:30:45 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+function talkAgain() {
+  alert("Hey Nics, I've been thinking, it would be wonderful to chat with you again. How about another conversation soon? Look, i really enjoy talking to you nics, i really do, i miss the old times na nag bo-bond tayo together. nagpaka-tanga ako for leaving you, i regret everything. i miss you so much, you can ignore me na uli after this. babyee");
+}
